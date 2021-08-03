466b3a882de0e3f501bcf95991c7e4016b9306da
jadergabriel <jadergabriel8@gmail.com>
2021-08-03 20:36:54 -0300

    Criação do DAO, Classes e Controles.

---
+++ CRUD_controle/user.php
@@ -0,0 +1,11 @@
+<?php
+
+class user
+{
+
+    //Cadastro de um novo USER
+    public function create($userDAO){
+
+
+    }
+}
---
+++ DAO/userDAO.php
@@ -0,0 +1,12 @@
+<?php
+
+class userDAO
+{
+    // Atributos do USUARIO dos sistema -> type será usado para definir ADMIN ou outros tipos de usuários, para fins de permissão.
+    public $id;
+    public $nome;
+    public $type;
+    public $status;
+
+
+}
